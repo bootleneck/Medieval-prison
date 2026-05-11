6d5ebb51f6d1cc4d279017f3fc920cb3506fa908
Soujir0 <david.guzman.del.moral@gmail.com>
2026-05-11 12:59:06 -0300

    Correcion de inventario
    se acomodo el icono del item que agarras para que inicie arriba a la izquierda y su tamaño. tambien se corrigio el error en el cual se generaban multiples slots y  que se generaba sin imagen.

--- Assets/Scripts/inventory system/ItemPickup.cs
+++ Assets/Scripts/inventory system/ItemPickup.cs
@@ -4,16 +4,28 @@ public class ItemPickup : MonoBehaviour
 {
     public ItemData item;
     public int amount = 1;
+    private bool hasBeenPickedUp = false; 
 
     private void OnTriggerEnter(Collider other)
     {
+        // ignora cualquier otra colisión
+        if (hasBeenPickedUp) return;
+
         if (other.CompareTag("Player"))
         {
+            hasBeenPickedUp = true; // Marcamos que ya se activó
+
             bool added = InventorySystem.Instance.AddItem(item, amount);
 
             if (added)
             {
                 Destroy(gameObject);
+            }
+            else
+            {
+                // Si el inventario estaba lleno, permitimos que se 
+                // pueda intentar recoger de nuevo luego.
+                hasBeenPickedUp = false;
             }
         }
     }
