5dc810c87dbb48236155b54705ef4c52b90e12f0
Soujir0 <david.guzman.del.moral@gmail.com>
2026-05-11 14:23:39 -0300

    Ajustes nuevos en inventario y equipamiento
    se logro acomodar los iconos y imagegenes en el iventario que no generen error, se agrego equipamiento con  1 y 2 de armas .  falta acomodar que salgan donde corresponde.

---
+++ Assets/Scripts/inventory system/EquipmentManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+    public static EquipmentManager Instance;
+    public Transform equipPoint;
+    private GameObject currentItemInHand;
+
+    private void Awake() { Instance = this; }
+
+    void Update()
+    {
+        // Si presionas 1, busca el primer ítem en el inventario
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            EquipByIndex(0);
+        }
+
+        // Si presionas 2, busca el segundo ítem
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            EquipByIndex(1);
+        }
+    }
+
+    void EquipByIndex(int index)
+    {
+        // Verificamos si hay algo en esa posición del inventario
+        if (InventorySystem.Instance.inventory.Count > index)
+        {
+            ItemData itemToEquip = InventorySystem.Instance.inventory[index].item;
+            Equip(itemToEquip);
+        }
+        else
+        {
+            Debug.Log("No hay ningún ítem en el slot " + (index + 1));
+        }
+    }
+
+    public void Equip(ItemData item)
+    {
+        if (currentItemInHand != null) Destroy(currentItemInHand);
+        if (item.visualPrefab == null) return;
+
+        currentItemInHand = Instantiate(item.visualPrefab, equipPoint);
+
+        currentItemInHand.transform.localPosition = Vector3.zero;
+        currentItemInHand.transform.localRotation = Quaternion.identity;
+
+        Debug.Log("Equipado: " + item.itemName);
+    }
+}
